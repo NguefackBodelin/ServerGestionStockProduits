89e90b0e2d256a65f85b4ca70b2bab9e9865dc15
NguefackBodelin <60115415+NguefackBodelin@users.noreply.github.com>
2020-09-15 09:27:38 +0200

    first commit

---
+++ src/main/java/org/gestionStockProduit/ServerGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package org.gestionStockProduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockProduitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gestionStockProduit/ServerGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestionStockProduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
